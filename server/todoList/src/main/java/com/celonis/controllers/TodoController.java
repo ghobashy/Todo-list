9e995bb4dc8d7db13450937f3e436e1982c4367c
Mohamed Ghobashy <mohamed.mostafa-ghobashy@vodafone.com@vodafone.com>
2017-08-19 14:38:02 +0200

    DEVELOP: Add Spring boot server app

---
+++ server/todoList/src/main/java/com/celonis/controllers/TodoController.java
@@ -0,0 +1,60 @@
+package com.celonis.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.celonis.entities.Todo;
+import com.celonis.services.ITodoService;
+
+@Controller
+@RequestMapping("todos")
+public class TodoController {
+
+	@Autowired
+	private ITodoService todoService;
+
+	@GetMapping("/")
+	public ResponseEntity<List<Todo>> getAllTodos() {
+		List<Todo> list = todoService.getAllTodos();
+		return new ResponseEntity<List<Todo>>(list, HttpStatus.OK);
+	}
+
+	@GetMapping("/{id}")
+	public ResponseEntity<Todo> getTodoById(@PathVariable("id") Integer id) {
+		Todo todo = todoService.getTodoById(id);
+		return new ResponseEntity<Todo>(todo, HttpStatus.OK);
+	}
+
+	@PostMapping("/")
+	public ResponseEntity<Void> addTodo(@RequestBody Todo todo) {
+		boolean flag = todoService.addTodo(todo);
+		if (flag == false) {
+			return new ResponseEntity<Void>(HttpStatus.CONFLICT);
+		}
+		return new ResponseEntity<Void>(HttpStatus.CREATED);
+	}
+	
+	@PatchMapping("/")
+	public ResponseEntity<Todo> updateArticle(@RequestBody Todo todo) {
+		todoService.updateTodo(todo);
+		return new ResponseEntity<Todo>(todo, HttpStatus.OK);
+	}
+	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Void> deleteArticle(@PathVariable("id") Integer id) {
+		todoService.deleteTodo(id);
+		return new ResponseEntity<Void>(HttpStatus.NO_CONTENT);
+	}
+
+}
---
+++ server/todoList/src/main/java/com/celonis/entities/Todo.java
@@ -0,0 +1,62 @@
+package com.celonis.entities;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+
+@Entity
+@Table(name="todo")
+public class Todo {
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	@Column(name="id")
+	private int id;
+	@Column(name="name")
+	private String name;
+	@Column(name="status")
+	private String status;
+	@Column(name="creation_date")
+	private Date creation_date;
+	@Column(name="user_id")
+	private int user_id;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	public int getUser_id() {
+		return user_id;
+	}
+	public void setUser_id(int user_id) {
+		this.user_id = user_id;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Date getCreation_date() {
+		return creation_date;
+	}
+	public void setCreation_date(Date creation_date) {
+		this.creation_date = creation_date;
+	}
+
+}
